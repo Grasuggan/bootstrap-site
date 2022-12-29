baf4ca248ff6066edfced798969c0dd58e5126a2
grasuggan <lena_reo@hotmail.com>
2022-12-29 16:52:42 +0100

    hero

--- js/custom.js
+++ js/custom.js
@@ -1,6 +1,5 @@
 var body = document.body;
 
-
 var hamburger = document.querySelector(".navbar-toggler");
 var offcanvasNavbar = hamburger.querySelectorAll("span");
 
@@ -10,4 +9,41 @@ var offcanvasNavbar = hamburger.querySelectorAll("span");
     });
    });
 
+   /*======================================
+      #Set .scroll class on scroll
+    ======================================*/
+    //Add the .scroll class to body when passing the io-hook placed over the header
+    var ioHook = document.querySelector(".io-hook");
+    if (ioHook) {
+        var observer = new IntersectionObserver(function (entries) {
+            if (entries[0].boundingClientRect.y < 0) {
+                body.classList.add('scroll');
+            }
+            else {
+                body.classList.remove('scroll');
+            }
+        });
+        observer.observe(ioHook);
+    }
 
+/*=============AUTOCPMPLETE SEARCH===================*/
+const autoCompleteJS = new autoComplete({
+    placeHolder: "Vad letar du efter?",
+    data: {
+        src: [
+            "Bad & simhall", 
+            "För anställda", 
+            "Återvinningscentral", 
+            "Läsårstider", 
+            "Arbeta", 
+            "Kommun & politik"]
+    },
+    resultItem: {
+        tag: "li",
+        class: "autoComplete_result",
+        element: (item, data) => {
+            var nodeItem =  `<a href="#">${item.innerText} <i class="bi bi-chevron-right" aria-hidden="true"></i></a>`;
+            item.innerHTML = nodeItem;
+        },
+    }
+});
