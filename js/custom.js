250b3c9face223ed4c36a7791fd99347fd3b3628
grasuggan <lena_reo@hotmail.com>
2023-02-01 15:42:00 +0100

    Adding arc to footer and pretty code

--- js/custom.js
+++ js/custom.js
@@ -1,57 +1,62 @@
 var body = document.body;
 
+/*==========================
+    #Mobile menu
+ ===========================*/
 var hamburger = document.querySelector(".navbar-toggler");
 var offcanvasNavbar = hamburger.querySelectorAll("span");
 
-   hamburger.addEventListener("click", function (){
-    offcanvasNavbar.forEach(function(span){
-        span.classList.toggle("show");
-    });
-   });
+hamburger.addEventListener("click", function () {
+  offcanvasNavbar.forEach(function (span) {
+    span.classList.toggle("show");
+  });
+});
 
-   /*======================================
+/*======================================
       #Set .scroll class on scroll
     ======================================*/
-    //Add the .scroll class to body when passing the io-hook placed over the header
-    var ioHook = document.querySelector(".io-hook");
-    if (ioHook) {
-        var observer = new IntersectionObserver(function (entries) {
-            if (entries[0].boundingClientRect.y < 0) {
-                body.classList.add('scroll');
-            }
-            else {
-                body.classList.remove('scroll');
-            }
-        });
-        observer.observe(ioHook);
+//Add the .scroll class to body when passing the io-hook placed over the header
+var ioHook = document.querySelector(".io-hook");
+if (ioHook) {
+  var observer = new IntersectionObserver(function (entries) {
+    if (entries[0].boundingClientRect.y < 0) {
+      body.classList.add("scroll");
+    } else {
+      body.classList.remove("scroll");
     }
+  });
+  observer.observe(ioHook);
+}
 
-/*=============AUTOCPMPLETE SEARCH===================*/
+/*==========================
+    #AUTOCPMPLETE SEARCH
+ ===========================*/
 const autoCompleteJS = new autoComplete({
-    placeHolder: "Vad letar du efter?",
-    data: {
-        src: [
-            "Bad & simhall", 
-            "För anställda", 
-            "Återvinningscentral", 
-            "Läsårstider", 
-            "Arbeta", 
-            "Kommun & politik"]
+  placeHolder: "Vad letar du efter?",
+  data: {
+    src: [
+      "Bad & simhall",
+      "För anställda",
+      "Återvinningscentral",
+      "Läsårstider",
+      "Arbeta",
+      "Kommun & politik",
+    ],
+  },
+  resultItem: {
+    tag: "li",
+    class: "autoComplete_result",
+    element: (item, data) => {
+      var nodeItem = `<a href="#">${item.innerText} <i class="bi bi-chevron-right" aria-hidden="true"></i></a>`;
+      item.innerHTML = nodeItem;
     },
-    resultItem: {
-        tag: "li",
-        class: "autoComplete_result",
-        element: (item, data) => {
-            var nodeItem =  `<a href="#">${item.innerText} <i class="bi bi-chevron-right" aria-hidden="true"></i></a>`;
-            item.innerHTML = nodeItem;
-        },
-    }
+  },
 });
 
 var navsearch = document.querySelectorAll(".input-nav .btn");
-navsearch.forEach(function (nav){
-    nav.addEventListener("click", function(){
-        var input = nav.closest("#search-autocomplete");
-        input.classList.toggle("open-input");
-    })
-})
+navsearch.forEach(function (nav) {
+  nav.addEventListener("click", function () {
+    var input = nav.closest("#search-autocomplete");
+    input.classList.toggle("open-input");
+  });
+});
